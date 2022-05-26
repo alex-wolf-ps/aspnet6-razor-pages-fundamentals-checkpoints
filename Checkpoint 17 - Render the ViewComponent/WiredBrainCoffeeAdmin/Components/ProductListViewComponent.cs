e9008d92fa740814840dc660736863c5f625956d
Alex Wolf <alexwolf@mcirosoft.com>
2022-05-25 20:39:13 -0400

    test

--- Checkpoint 17 - Render the ViewComponent/WiredBrainCoffeeAdmin/Components/ProductListViewComponent.cs
+++ Checkpoint 17 - Render the ViewComponent/WiredBrainCoffeeAdmin/Components/ProductListViewComponent.cs
@@ -3,24 +3,13 @@ using WiredBrainCoffeeAdmin.Data;
 
 namespace WiredBrainCoffeeAdmin.Components
 {
-    public class ProductListViewComponent : ViewComponent
+    public class ProductListViewComponent
     {
         private IProductRepository productRepo;
 
         public ProductListViewComponent(IProductRepository productRepository)
         {
             this.productRepo = productRepository;
-        }
-
-        public IViewComponentResult Invoke(int count)
-        {
-            var items = productRepo.GetAll();
-            if(count > 0)
-            {
-                return View(items.Take(count).ToList());
-            }
-            
-            return View(items);
         }
     }
 }
