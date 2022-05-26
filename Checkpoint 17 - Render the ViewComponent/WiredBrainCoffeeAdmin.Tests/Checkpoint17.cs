8f1f8eecab094f86e0e98ef154b84d323db076d5
Alex Wolf <alexwolf@mcirosoft.com>
2022-05-25 20:35:47 -0400

    checkpoint 17

---
+++ Checkpoint 17 - Render the ViewComponent/WiredBrainCoffeeAdmin.Tests/Checkpoint17.cs
@@ -0,0 +1,31 @@
+using HtmlAgilityPack;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace WiredBrainCoffeeAdmin.Tests
+{
+    public class Checkpoint17
+    {
+        [Fact]
+        public void M03_VerifyRazorConditional()
+        {
+            var filePath = TestHelpers.GetRootString() + "WiredBrainCoffeeAdmin"
+                + Path.DirectorySeparatorChar + "Pages"
+                + Path.DirectorySeparatorChar + "Index.cshtml";
+
+            Assert.True(File.Exists(filePath), "`Index.cshtml` should exist in the Pages folder.");
+
+            var doc = new HtmlDocument();
+            doc.Load(filePath);
+
+            Assert.True(doc.Text.Contains("@await Component.InvokeAsync(\"ProductList\")")
+                || doc.Text.Contains("@await  Component.InvokeAsync(\"ProductList\")")
+                || doc.Text.Contains("@await Component.InvokeAsync(\"ProductList\")")
+                || doc.Text.Contains("@await Component.InvokeAsync( \"ProductList\")")
+                || doc.Text.Contains("@await Component.InvokeAsync(\"ProductList\")" ),
+                "The index page does not include code to display a view component.");
+        }
+    }
+}
