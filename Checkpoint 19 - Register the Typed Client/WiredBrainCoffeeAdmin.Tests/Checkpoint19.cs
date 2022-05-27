e6afd696fe230f98abc04be4c03d4e181189f030
Alex Wolf <alexwolf@mcirosoft.com>
2022-05-27 18:06:54 -0400

    update

---
+++ Checkpoint 19 - Register the Typed Client/WiredBrainCoffeeAdmin.Tests/Checkpoint19.cs
@@ -0,0 +1,26 @@
+using HtmlAgilityPack;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace WiredBrainCoffeeAdmin.Tests
+{
+    public class Checkpoint19
+    {
+        [Fact]
+        public void CH18_VerifyHttpClientRegistration()
+        {
+            var filePath = TestHelpers.GetRootString() + "WiredBrainCoffeeAdmin"
+                + Path.DirectorySeparatorChar + "Program.cs";
+
+            Assert.True(File.Exists(filePath), "`Program.cs` should exist in the project root.");
+
+            var doc = new HtmlDocument();
+            doc.Load(filePath);
+
+            Assert.True(doc.Text.Replace(" ", "").Contains("builder.Services.AddHttpClient<ITicketService,TicketService>()"),
+                "`Program.cs` should include code to register a typed HttpClient for the ticket service.");
+        }
+    }
+}
---
+++ Checkpoint 19 - Register the Typed Client/WiredBrainCoffeeAdmin/Program.cs
@@ -0,0 +1,41 @@
+using WiredBrainCoffeeAdmin.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<WiredContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("WiredBrain")));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddHttpClient();
+
+// TODO: Register the typed HTTPClient here
+
+var app = builder.Build();
+
+await EnsureDbCreated(app.Services, app.Logger);
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+async Task EnsureDbCreated(IServiceProvider services, ILogger logger)
+{
+    using var db = services.CreateScope()
+        .ServiceProvider.GetRequiredService<WiredContext>();
+    await db.Database.MigrateAsync();
+}
